3283c70c433e5874088b37c8bca34a12d54ad61f
Rick Lan <ricklan@gmail.com>
2025-12-23 13:34:24 +0800

    no more tx

--- opendbc/safety/modes/byd.h
+++ opendbc/safety/modes/byd.h
@@ -110,8 +110,10 @@ static bool byd_tx_hook(const CANPacket_t *msg) {
 static safety_config byd_init(uint16_t param) {
   SAFETY_UNUSED(param);
 
-  // Pure forwarding mode - no blocking, no sending
-  static const CanMsg BYD_TX_MSGS[] = {};
+  // Allow LKAS TX but don't block stock LKAS (check_relay=false)
+  static const CanMsg BYD_TX_MSGS[] = {
+    {BYD_LKAS, BYD_MAIN, 8, .check_relay = false},
+  };
 
   static RxCheck byd_rx_checks[] = {
     // Steering angle sensor (main bus, 50Hz)

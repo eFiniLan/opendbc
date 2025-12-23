5dc2bc2bf08953dc65ef97e4efe7a5cd74c7eb9c
Rick Lan <ricklan@gmail.com>
2025-12-23 11:01:20 +0800

    clean up

--- opendbc/car/byd/values.py
+++ opendbc/car/byd/values.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 
-from opendbc.car.lateral import AngleSteeringLimits, ISO_LATERAL_ACCEL
-from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, CarSpecs, DbcDict, PlatformConfig, Platforms, structs
+from opendbc.car.lateral import AngleSteeringLimits
+from opendbc.car import Bus, CarSpecs, DbcDict, PlatformConfig, Platforms, structs
 from opendbc.car.docs_definitions import CarHarness, CarDocs, CarParts
 from opendbc.car.structs import CarParams
 from opendbc.car.fw_query_definitions import FwQueryConfig, Request, StdQueries
